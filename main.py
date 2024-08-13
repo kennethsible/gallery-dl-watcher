910afbe71fed071ce8349e7b2de898f8e3d74c25
Ken Sible <ksible@outlook.com>
2024-08-13 12:56:52 -0400

    fixed incorrect download count from os.listdir

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+# import datetime
 import json
 import os
 import sys
@@ -12,15 +12,15 @@ import schedule
 # https://discordapp.com/developers/docs/resources/channel#embed-object
 
 
-def timestamp() -> str:
-    timestamp = datetime.datetime.now()
-    tz = pytz.timezone(os.environ['TZ'])
-    timestamp = timestamp.astimezone(tz)
-    return timestamp.strftime('%x %X')
+# def timestamp() -> str:
+#     timestamp = datetime.datetime.now()
+#     tz = pytz.timezone(os.environ['TZ'])
+#     timestamp = timestamp.astimezone(tz)
+#     return timestamp.strftime('%x %X')
 
 
 def webhook(content: str):
-    print(timestamp(), '[INFO]', content)
+    print('[INFO]', content)
     url = os.environ['WEBHOOK_URL']
     if len(url) > 0:
         data = {"content": content}  # "username" : ""
@@ -29,34 +29,34 @@ def webhook(content: str):
         try:
             result.raise_for_status()
         except requests.exceptions.HTTPError as error:
-            print(timestamp(), '[ERROR]', error, file=sys.stderr)
+            print('[ERROR]', error, file=sys.stderr)
         else:
-            print(timestamp(), '[INFO] Status Code', result.status_code)
+            print('[INFO] Webhook Status Code', result.status_code)
 
 
 def gallery_dl():
     with open('gallery-dl/config.json') as config_f:
         config = json.load(config_f)
+    print('[INFO] Monitoring Session Started.')
     for url in config:
         path, users = config[url]
         for user_id in users:
             args = ' '.join(users[user_id])
-            count_i = count_f = 0
-            if os.path.exists(f'/downloads/{path}/{user_id}'):
-                count_i = len(
-                    [x for x in os.listdir(f'/downloads/{path}/{user_id}') if os.path.isfile(x)]
-                )
+            dl_path = f'/downloads/{path}/{user_id}'
+            count_i = 0
+            if os.path.exists(dl_path):
+                count_i = len([x for x in os.listdir(dl_path) if os.path.isfile(dl_path + '/' + x)])
             os.system(f'gallery-dl {url}{user_id} -d /downloads {args}')
-            if os.path.exists(f'/downloads/{path}/{user_id}'):
-                count_f = len(
-                    [x for x in os.listdir(f'/downloads/{path}/{user_id}') if os.path.isfile(x)]
-                )
-            count_diff = '' if count_i == count_f == 0 else str(count_f - count_i) + ' '
-            webhook(f'gallery-dl finished downloading {count_diff}image(s) from {path}/{user_id}.')
+            count_str = ''
+            if os.path.exists(dl_path):
+                count_f = len([x for x in os.listdir(dl_path) if os.path.isfile(dl_path + '/' + x)])
+                count_str = str(count_f - count_i) + ' '
+            webhook(f'Downloaded {count_str}Image(s) from {path}/{user_id}.')
+    print('[INFO] Monitoring Session Finished.')
 
 
 def main():
-    print(timestamp(), '[INFO] gallery-dl Started.')
+    print('[INFO] Application Initialized.')
     if os.environ['ONCE_ON_STARTUP'] == 'true':
         gallery_dl()
     schedule_time = os.environ['SCHEDULE_TIME']
